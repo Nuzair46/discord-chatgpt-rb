bb6639efb0c9f089e53708e55b67dcc8e4e4f677
Nuzair <nuzair.rasheed@bigbinary.com>
2023-01-02 19:04:23 +0530

    added help

--- bot.rb
+++ bot.rb
@@ -4,17 +4,17 @@ require 'discordrb'
 require 'ruby/openai'
 
 # Set up a Discord client and authenticate with your bot token
-client = Discordrb::Commands::CommandBot.new(token: ENV['DISCORD_TOKEN'], prefix: '!', help_command: false)
+client = Discordrb::Commands::CommandBot.new(token: ENV['DISCORD_TOKEN'], prefix: '!')
 
 client.ready do
-  client.stream('Red', 'https://rednek46.me')
+  client.stream('Red | !help', 'https://rednek46.me')
 end
 
 # Set up the OpenAI API client with your API key
 openai_client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
 
 # Define a command that generates a response using ChatGPT
-client.command(:chat) do |event, *prompt|
+client.command(:chat, description: 'Chat with ChatGPT') do |event, *prompt|
   # Use the OpenAI API to generate a response
 
   response = openai_client.completions(
@@ -30,7 +30,7 @@ client.command(:chat) do |event, *prompt|
 end
 
 # use dalle to generate images
-client.command(:generate) do |event, *prompt|
+client.command(:generate, description: 'Generate image with DALLE2')) do |event, *prompt|
   response = openai_client.images.generate(parameters: { prompt: prompt.join(' ') })
   event.respond response.dig('data', 0, 'url')
 end
