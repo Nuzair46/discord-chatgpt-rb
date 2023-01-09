e6950688fb983a822d5a6d4ba8e6b42280212e1a
Nuzair <nuzair.rasheed@bigbinary.com>
2023-01-09 14:32:23 +0530

    Added rate limitting and whitelist

--- bot.rb
+++ bot.rb
@@ -7,14 +7,20 @@ require 'ruby/openai'
 client = Discordrb::Commands::CommandBot.new(token: ENV['DISCORD_TOKEN'], prefix: '!')
 
 client.ready do
-  client.stream('Red | !help', 'https://rednek46.me')
+  client.game 'by Red | !help', 'https://rednek46.me'
 end
+
+# setting up bucket
+rate_limitter = Discordrb::Commands::SimpleRateLimiter.new
+rate_limitter.bucket :api_limit, limit: 10, time_span: 1.day.seconds.to_i, delay: 10.minutes.seconds.to_i
 
 # Set up the OpenAI API client with your API key
 openai_client = OpenAI::Client.new(access_token: ENV['OPENAI_API_KEY'])
 
 # Define a command that generates a response using ChatGPT
 client.command(:chat, description: 'Chat with ChatGPT') do |event, *prompt|
+  next if rate_limited?(event)
+
   # Use the OpenAI API to generate a response
 
   response = openai_client.completions(
@@ -35,10 +41,30 @@ end
 
 # use dalle to generate images
 client.command(:generate, description: 'Generate image with DALLE2') do |event, *prompt|
+  next if rate_limited?(event)
+
   response = openai_client.images.generate(parameters: { prompt: prompt.join(' ') })
   event.message.reply! response.dig('data', 0, 'url')
 rescue RestClient::BadRequest
   event.message.reply! 'Image generation failed, Probably you asked something too weird.'
+end
+
+def rate_limited?(event)
+  return false if skip_rate_limit?(event)
+
+  rate_limiter.rate_limited?(:api_limit, event.user)
+end
+
+def skip_rate_limit?(event)
+  owner?(event.user.id) || rate_limit_server_whitelist.include?(event.server.id)
+end
+
+def owner?(user_id)
+  user_id == ENV['OWNER_ID']
+end
+
+def rate_limit_server_whitelist(server_id)
+  ENV['RATE_LIMIT_SERVER_WHITELIST'].split(',').include?(server_id)
 end
 
 # Run the bot
